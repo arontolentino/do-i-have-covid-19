28796c4251af30b11004de97d2632ce3f618b71f
Aron Tolentino <arontolentino@outlook.com>
2020-03-15 09:59:00 -0400

    Create react app and set up Bulma

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'bulma/css/bulma.css';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<section class="section">
+				<div class="container">
+					<h1 class="title">Hello World</h1>
+					<p class="subtitle">
+						My first website with <strong>Bulma</strong>!
+					</p>
+				</div>
+			</section>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+	state = {};
+	render() {
+		return <h1>Nav</h1>;
+	}
+}
+
+export default Nav;
